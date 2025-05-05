163244fd8373a9c1fa574f0a8081fca7cf7fdee7
Knative Prow Updater Robot <automation+prow-updater-robot@knative.team>
2025-05-05 15:15:38 +0000

    Bumping Prow and Boskos
    Multiple distinct us-docker.pkg.dev/k8s-infra-prow/images/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes-sigs/prow/compare/fcccd15fe...d852aabe5 | 2025&#x2011;04&#x2011;25&nbsp;&#x2192;&nbsp;2025&#x2011;05&#x2011;03 | images/branchprotector, images/checkconfig, images/cherrypicker, images/clonerefs, images/config-bootstrapper, images/crier, images/deck, images/entrypoint, images/generic-autobumper, images/ghproxy, images/hook, images/horologium, images/initupload, images/mkpj, images/needs-rebase, images/peribolos, images/prow-controller-manager, images/sidecar, images/sinker, images/status-reconciler, images/tide
    
    No gcr.io/k8s-staging-boskos/ changes.

--- prow/jobs/run_job.sh
+++ prow/jobs/run_job.sh
@@ -39,7 +39,7 @@ if [[ -n "${GITHUB_TOKEN_PATH}" ]]; then
     -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${JOB_CONFIG_YAML}:${JOB_CONFIG_YAML}" \
     -v "${GITHUB_TOKEN_PATH}:${GITHUB_TOKEN_PATH}" \
     -w "${PWD}" \
-    us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250425-fcccd15fe \
+    us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250503-d852aabe5 \
     "--job=${JOB_NAME}" "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_CONFIG_YAML}" \
     "--github-token-path=${GITHUB_TOKEN_PATH}" \
     >"${JOB_YAML}"
@@ -48,7 +48,7 @@ else
   docker run -i --rm \
     -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${JOB_CONFIG_YAML}:${JOB_CONFIG_YAML}" \
     -w "${PWD}" \
-    us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250425-fcccd15fe \
+    us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250503-d852aabe5 \
     "--job=${JOB_NAME}" "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_CONFIG_YAML}" \
     >"${JOB_YAML}" || failed=1
 
